4894d746ec93dbcd91d2bc3bb82dd8c037cd1288
abdullah107189 <abdullah107189@gmail.com>
2025-02-20 17:44:25 +0600

    show the delete and edit button and fixed overflow text

--- src/conponents/TaskBody.jsx
+++ src/conponents/TaskBody.jsx
@@ -1,4 +1,6 @@
+import { AiOutlineDelete } from "react-icons/ai";
 import useGetTask from "../hooks/useGetTask";
+import { IoPencilOutline } from "react-icons/io5";
 
 const TaskBody = () => {
   const { data } = useGetTask();
@@ -7,12 +9,21 @@ const TaskBody = () => {
       {data?.map((task) => (
         <div
           key={task?._id}
-          className="border p-2 rounded-2xl border-gray-200 shadow-md bg-white"
+          className="border p-2 group rounded-2xl border-gray-200 shadow-md bg-white relative"
         >
+          {console.log(task?.description.toString())}
+          <div className="hidden group-hover:flex transform duration-200">
+            <button className="border p-1 rounded-full cursor-pointer mr-2 absolute -top-4 right-0 hover:bg-gray-100 bg-white border-gray-400">
+              <AiOutlineDelete />
+            </button>
+            <button className="border p-1 rounded-full cursor-pointer mr-2 absolute -top-4 right-8 hover:bg-gray-100 bg-white border-gray-400">
+              <IoPencilOutline />
+            </button>
+          </div>
           <h1 className="text-xl pb-1 border-b border-gray-300 ">
             {task?.title}
           </h1>
-          <p>{task?.description}</p>
+          <p className="break-words">{task?.description}</p>
         </div>
       ))}
     </div>
